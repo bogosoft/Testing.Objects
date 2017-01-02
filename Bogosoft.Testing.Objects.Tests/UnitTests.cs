0dfbb972d7ba9643af9e04c75c641b723670ca4f
Clayton Roth <croth0627@yahoo.com>
2017-01-01 19:28:16 -0800

    Last minute update.

--- Bogosoft.Testing.Objects.Tests/UnitTests.cs
+++ Bogosoft.Testing.Objects.Tests/UnitTests.cs
@@ -1,10 +1,6 @@
 ﻿using NUnit.Framework;
 using Should;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bogosoft.Testing.Objects.Tests
 {
